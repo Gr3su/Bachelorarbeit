fc573af96c8b877747064d81919d7799fe08ac16
gresu <yannickgross@outlook.de>
2025-07-12 13:16:16 +0200

    ordnerstruktur zum schreiben und lesen bei vorbereitung

--- PythonSurvey/DataPreparation.py
+++ PythonSurvey/DataPreparation.py
@@ -4,15 +4,18 @@ import numpy as np
 def readFiles(paths : list[str]):
     contents = []
     for filepath in paths:
-        with open(filepath, 'r', encoding='utf-8', errors='ignore') as f:
+        with open(filepath, 'r', encoding='utf-8') as f:
             contents.append(f.read())
     return contents
 
 def writeFiles(paths : list[str], contents):
     for path, content in zip(paths, contents):
-        with open(path, 'w', encoding='utf-8', errors='ignore') as f:
-            f.write(str(content)[1:-1])
+        newFileName = os.path.splitext(os.path.basename(path))[0] + ".csv"
+        newPath = os.path.join(os.path.dirname(path), "../prepared", newFileName)
 
+        os.makedirs(os.path.dirname(newPath), exist_ok=True)
+        with open(newPath, 'w', encoding='utf-8') as f:
+            f.write(str(content))
 
 def nvidiaData(contents: list[str]):
     for i, data in enumerate(contents):
@@ -21,9 +24,39 @@ def nvidiaData(contents: list[str]):
             float(line.split(",")[1]) - float(line.split(",")[4])
             for line in lines
         ]
-        contents[i] = results
+        contents[i] = str(results)[1:-1]
 
+def euWeatherData(contents: list[str], full_paths: list[str]):
+    valid_contents = []
+    valid_paths = []
 
+    for data, path in zip(contents, full_paths):
+        lines = data.strip().splitlines()
+        temp_idx = qual_idx = None
+
+        for i, line in enumerate(lines):
+            if "TG   : Mean temperature in" in line:
+                start, end = map(int, line.split()[0].split("-"))
+                temp_idx = (start, end)
+            elif "Q_TG : quality code for TG" in line:
+                start, end = map(int, line.split()[0].split("-"))
+                qual_idx = (start, end)
+            elif line.startswith("STAID"):
+                lines = lines[i+1:]
+                break
+
+        if temp_idx is None or qual_idx is None:
+            continue
+
+        if any(int(line[qual_idx[0]:qual_idx[1]]) != 0 for line in lines):
+            continue
+
+        temps = [line[temp_idx[0]:temp_idx[1]].strip() for line in lines]
+        valid_contents.append(", ".join(temps))
+        valid_paths.append(path)
+
+    contents[:] = valid_contents
+    full_paths[:] = valid_paths
 
 
 if __name__ == "__main__":
@@ -36,7 +69,9 @@ if __name__ == "__main__":
             nvidiaData(contents)
             writeFiles(full_paths, contents)
         case 1:
-            pass
+            contents = readFiles(full_paths)
+            euWeatherData(contents, full_paths)
+            writeFiles(full_paths, contents)
         case 2:
             pass
         case 3:
