5d284cc74529cfb41ec264532e92da1c7b5ee75e
gresu <yannickgross@outlook.de>
2025-07-13 18:40:59 +0200

    lesen, komprimieren und schreiben von prepared daten

--- PythonSurvey/CompressionReader.py
+++ PythonSurvey/CompressionReader.py
@@ -1,6 +1,10 @@
-import sys
+import sys, os
 
-from PythonSurvey.Utilities import MultivariateTimeSeries as tss
+from Utilities import MultivariateTimeSeries as tss
+import CompressionAlgorithms as comp
+import AnomalyAlgorithms as anom
+import numpy as np
+import re
 
 def formatCheck(lines):
     if not lines:
@@ -8,30 +12,46 @@ def formatCheck(lines):
     for i in lines:
         if not i:
             raise ValueError("Die Datei darf keine Leerzeile enthalten.")
-        if not isFloat(i):
+        if not isFloatArray(i):
             raise ValueError(f"Eine Zeile enthaelt keine Zahl, sondern folgendes: {i}")
 
-def isFloat(value):
-    try:
-        float(value)
+def isFloatArray(value):
+    pattern = r'^(-?\d+(?:\.\d+)?)(,\s*-?\d+(?:\.\d+)?)*$'
+    if re.fullmatch(pattern, value):
         return True
-    except ValueError:
-        return False
+    return False
+    
+def writeFiles(paths : list[str], pathExtension, contents):
+    for path, content in zip(paths, contents):
+        newFileName = os.path.splitext(os.path.basename(path))[0] + ".csv"
+        newPath = os.path.join(os.path.dirname(path), "../compressed/" + pathExtension, newFileName)
+
+        os.makedirs(os.path.dirname(newPath), exist_ok=True)
+        with open(newPath, 'w', encoding='utf-8') as f:
+            f.write(str(content)[1:-1])
 
 if __name__ == "__main__":
+    full_paths = [os.path.join(sys.argv[1], filename) for filename in os.listdir(sys.argv[1])]
+    full_paths = [path for path in full_paths if os.path.isfile(path)]
+
     timeSeries = []
-    for i in sys.argv:
-        with open(i, "r") as f:
+    for path in full_paths:
+        with open(path, "r") as f:
             lines = f.readlines()
-
-        try:
             formatCheck(lines)
-        except ValueError as e:
-            print(e)
-            break
 
-        timeSeries.append([float(x) for x in lines])
+            timeSeries.append([float(x) for x in lines[0].strip().split(",")])
+
     checkedTimeSeries = tss(timeSeries)
 
-    for i in range(0,5):
-        pass
+    linTS = comp.linearApproximation(checkedTimeSeries, int(sys.argv[2]))
+    writeFiles(full_paths, "linearApprox", linTS)
+    polTS = comp.polynomialApproximation(checkedTimeSeries, int(sys.argv[3]))
+    writeFiles(full_paths, "polyApprox", polTS)
+    dwtTS = comp.dwtApproximation(checkedTimeSeries, int(sys.argv[4]))
+    writeFiles(full_paths, "waveletApprox", dwtTS)
+    dftTS = comp.dftApproximation(checkedTimeSeries, int(sys.argv[5]) if len(sys.argv) == 6 else 10)
+    writeFiles(full_paths, "fourierApprox", dftTS)
+
+    #for i in (linTS, polTS, dwtTS, dftTS):
+        
