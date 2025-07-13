403f60023427f410ee83d87dc253c2ddda7b75c3
gresu <yannickgross@outlook.de>
2025-07-13 18:40:23 +0200

    umbenennung von timeSeriesLength

--- PythonSurvey/CompressionAlgorithms.py
+++ PythonSurvey/CompressionAlgorithms.py
@@ -1,7 +1,7 @@
 from Utilities import MultivariateTimeSeries as tss
 from numpy.polynomial.polynomial import Polynomial as Pol
 from numpy.fft import fft
-from numpy import abs
+from numpy import abs, percentile
 import pywt
 
 def checkParameters(multivariateTimeSeries : tss, intParameter : int):
@@ -16,7 +16,7 @@ def linearApproximation(multivariateTimeSeries : tss, segmentLength : int):
     compressedMultivariateTimeSeries = []
     for i in multivariateTimeSeries.multivariateTimeSeries:
         y_fits = []
-        for j in range(0, multivariateTimeSeries.timeSeries_length, segmentLength):
+        for j in range(0, multivariateTimeSeries.timeSeriesLength, segmentLength):
             # hier in der arbeit erwähnen dass die x werte keine Rolle spielen, außer dass sie den gleichen abstand haben, weil bei der datenauswahl gleichmäßige intervalle gewählt werden
             rightBoundary = j + segmentLength if j + segmentLength <= len(i) else len(i)
             # und hier erwähnen dass die Verfälschung vom letzten Segment keine Rolle spielt, weil es bei jeder Zeitreihe verfälscht wird
@@ -32,7 +32,7 @@ def polynomialApproximation(multivariateTimeSeries : tss, segmentLength : int):
     compressedMultivariateTimeSeries = []
     for i in multivariateTimeSeries.multivariateTimeSeries:
         coefficients = []
-        for j in range(0, multivariateTimeSeries.timeSeries_length, segmentLength):
+        for j in range(0, multivariateTimeSeries.timeSeriesLength, segmentLength):
             rightBoundary = j + segmentLength if j + segmentLength <= len(i) else len(i)
             p = Pol.fit(range(j, rightBoundary), i[j:rightBoundary], deg=3)
             coefficients.append(p.convert().coef)
@@ -52,13 +52,14 @@ def dwtApproximation(multivariateTimeSeries : tss, iterations : int):
         compressedMultivariateTimeSeries.append(data)
     return compressedMultivariateTimeSeries
 
-def dftApproximation(multivariateTimeSeries : tss, threshold : int):
-    checkParameters(multivariateTimeSeries, threshold)
+def dftApproximation(multivariateTimeSeries : tss, keepPercentile : int):
+    checkParameters(multivariateTimeSeries, keepPercentile)
 
     compressedMultivariateTimeSeries = []
     for i in multivariateTimeSeries.multivariateTimeSeries:
-        freq = len(i)
         sp = fft(i)
+        sp = sp[:len(sp) // 2]
+        threshold = percentile(abs(sp), 100 - keepPercentile)
         sp[abs(sp) < threshold] = 0
         compressedMultivariateTimeSeries.append(sp)
     return compressedMultivariateTimeSeries
--- PythonSurvey/Utilities.py
+++ PythonSurvey/Utilities.py
@@ -2,21 +2,21 @@ import numpy as np
 
 class MultivariateTimeSeries:
     def addTimeSeries(self, timeSeries):
-        if not isinstance(timeSeries, np.ndarray):
-            raise TypeError(f"Erwarte ein NumPy-Array, aber ein {type(timeSeries)} erhalten.")
+        if not isinstance(timeSeries, list):
+            raise TypeError(f"Erwarte eine List, aber ein {type(timeSeries)} erhalten.")
         
-        if len(timeSeries) != self.timeSeries_length:
-            raise ValueError(f"Das Segment hat {len(timeSeries)} Element(e), muss aber {self.timeSeries_length} haben.")
+        if len(timeSeries) != self.timeSeriesLength:
+            raise ValueError(f"Das Segment hat {len(timeSeries)} Element(e), muss aber {self.timeSeriesLength} haben.")
         
-        self.multivariateTimeSeries = np.append(self.multivariateTimeSeries, timeSeries)
+        self.multivariateTimeSeries.append(timeSeries)
     
     def __init__(self, timeSeries):
         if not isinstance(timeSeries, list):
             raise TypeError(f"Erwarte eine List, aber ein {type(timeSeries)} erhalten.")
-        if not isinstance(timeSeries[0], np.ndarray):
-            raise TypeError(f"Erwarte ein NumPy-Array, aber ein {type(timeSeries[0])} erhalten.")
+        if not isinstance(timeSeries[0], list):
+            raise TypeError(f"Erwarte eine List, aber ein {type(timeSeries[0])} erhalten.")
         
-        self.multivariateTimeSeries = np.array(timeSeries[0])
-        self.timeSeries_length = len(timeSeries[0])
+        self.multivariateTimeSeries = [timeSeries[0]]
+        self.timeSeriesLength = len(timeSeries[0])
         for i in range(1,len(timeSeries)):
             self.addTimeSeries(timeSeries[i])
